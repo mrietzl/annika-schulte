3268da3dfb2a8dd8640835f320fc5ee17a491166
mrietzl <michele@familie-rietzl.de>
2023-10-22 22:16:44 +0200

    totally in my web flow

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,8 @@
+// adding the prettier-plugin-tailwindcss to this prettier.config.js
 module.exports = {
   plugins: ["prettier-plugin-tailwindcss"],
+};
+
+module.exports = {
+  tailwindConfig: "./styles/tailwind.config.js",
 };
